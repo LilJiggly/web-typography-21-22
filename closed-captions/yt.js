0f98c974c27b0fba3c888dda1e7052f5281c479b
LilJiggly <58260324+LilJiggly@users.noreply.github.com>
2022-05-18 22:54:00 +0200

    Add files via upload

--- closed-captions/yt.js
+++ closed-captions/yt.js
@@ -2,8 +2,8 @@ var player;
 
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('video-placeholder', {
-		width: 600,
-		height: 400,
+		width: 800,
+		height: 350,
 		videoId: 'vrP-_T-h9YM',
 		playerVars: {
 			color: 'white',
